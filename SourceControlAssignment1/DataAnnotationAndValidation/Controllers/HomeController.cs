bfee752acf5a76aedc6ab0ee8b34ffb8a7a9d411
Shubham <slunker270@gmail.com>
2020-11-27 12:41:04 +0530

    Added

---
+++ SourceControlAssignment1/DataAnnotationAndValidation/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using DataAnnotationAndValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataAnnotationAndValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Details(RegistrationModel sm)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.name = sm.Name;
+                ViewBag.mobile = sm.Mobile;
+                ViewBag.email = sm.Email;
+                ViewBag.age = sm.Age;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.name = "No Data";
+                ViewBag.mobile = "No Data";
+                ViewBag.email = "No Data";
+                ViewBag.age = "No Data";
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ SourceControlAssignment1/DataAnnotationAndValidation/Models/RegistrationModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataAnnotationAndValidation.Models
+{
+    [Bind(Exclude = "Id")]
+    public class RegistrationModel
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is mandatory")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email ID is mandatory")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Incorrect Email Format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Confirm Email is mandatory")]
+        [DataType(DataType.EmailAddress)]
+        [System.ComponentModel.DataAnnotations.Compare("Email", ErrorMessage = "Email Not Matched")]
+        public string ConfirmEmail { get; set; }
+
+        //[Required(ErrorMessage = "Age is mandatory")]
+        //[Range(1, 120, ErrorMessage = "Age must be between 1-120 in years.")]
+        //public int Age { get; set; }
+
+        [Required(ErrorMessage = "Age is Required")]
+        [CustomAttribute.MinAge(18)]
+        public int Age { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{2})[-. ]?([0-9]{4})[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Not a valid Phone number")]
+        public string Mobile { get; set; }
+    }
+}
